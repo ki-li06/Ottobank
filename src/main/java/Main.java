0215e15a713d319397cdd14bf19de7a34cb19e43
Kilian <kilian@lindmeyr.com>
2023-06-14 21:07:03 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        delay(200L);
+        System.out.println("Gut reicht auch wieder");
+        
+    }
+    private static void delay (long millis){
+        try{
+            Thread.sleep(millis);
+        }
+        catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+    }
+}

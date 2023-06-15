4731a06fa40f6fb4ca1f5b4e211c88415866ca37
Kilian <kilian@lindmeyr.com>
2023-06-15 22:30:10 +0200

    dc webhook test

--- src/main/java/Datenbank/Datenbank.java
+++ src/main/java/Datenbank/Datenbank.java
@@ -3,6 +3,6 @@ package Datenbank;
 public class Datenbank {
     public static void main(String[] args) {
         System.out.println("Datenbank sagt hallo");
-
+        System.out.println("und wieder tschau");
     }
 }

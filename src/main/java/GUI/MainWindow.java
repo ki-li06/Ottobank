7ca267716a341c4a751dadc069fc77175157da78
Anonym2546 <johannes.25406@gmail.com>
2023-06-15 12:03:04 +0200

    Added GUI

---
+++ src/main/java/GUI/MainWindow.java
@@ -0,0 +1,26 @@
+package GUI;
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import javax.swing.*;
+
+
+public class MainWindow {
+    JFrame f;
+    MainWindow(){
+        FlatDarkLaf.setup();
+        f=new JFrame();//creating instance of JFrame
+
+        JButton b=new JButton("click");//creating instance of JButton
+        b.setBounds(130,100,100, 40);
+
+        f.add(b);//adding button in JFrame
+
+        f.setSize(400,500);//400 width and 500 height
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
+    }
+
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+}

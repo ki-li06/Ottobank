14e528c983f779c95233d9555ad4108245092320
Jonas Schlick <jonasfschlick@gmail.com>
2023-06-15 13:02:28 +0200

    Test 2

---
+++ src/main/java/GUI/UIInputListener.java
@@ -0,0 +1,5 @@
+package GUI;
+//Use this class to communicate with Input or Dropdown menus. It is called if a value changes.
+public abstract class UIInputListener {
+    public abstract String InputChanged();
+}

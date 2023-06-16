7ea9f90200e36c7058389b91c3746bf74aa1d706
Anonym2546 <johannes.25406@gmail.com>
2023-06-16 11:22:37 +0200

    Implemented Stacked Widget test

--- src/main/java/GUI/MainWindow.java
+++ src/main/java/GUI/MainWindow.java
@@ -1,6 +1,7 @@
 package GUI;
 import com.formdev.flatlaf.FlatDarkLaf;
 
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,34 +12,37 @@ public class MainWindow {
         FlatDarkLaf.setup();
         f=new JFrame();//creating instance of JFrame
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.setSize(500, 500);
-        JLayeredPane pages = new JLayeredPane();
-        pages.setPreferredSize(new Dimension(400, 400));
-        BetterTextField a = new BetterTextField(new UITextFieldMethod() {
+        f.setSize(850, 500);
+        f.setResizable(false);
+        f.setLayout(null);
+
+        StackedWidget pages = new StackedWidget();
+        pages.setBounds(f.getBounds());
+        StackedPane a = new StackedPane(f);
+        StackedPane b = new StackedPane(f);
+        StackedPane c = new StackedPane(f);
+        pages.registerPane(a);
+        pages.registerPane(b);
+        pages.registerPane(c);
+
+        BetterTextField d = new BetterTextField(new UITextFieldMethod() {
             @Override
             public void performMethod(String data) {
                 System.out.println(data);
             }
         });
-        pages.add(a, 2);
-        a.setBounds(0,0,200,20);
-        JPanel backgroundPanel = new JPanel();
-        backgroundPanel.setBackground(Color.RED);
-        backgroundPanel.setBounds(0, 0, 400, 400);
-        pages.add(backgroundPanel, Integer.valueOf(0)); // Add to layer 0
+        BetterButton e = new BetterButton(new UIButtonMethod() {
+            @Override
+            public void performMethod() {
+                pages.showPlane(1);
+            }
+        });
+        e.setText("Switch to next Page");
+        a.registerComponent(e);
+        b.registerComponent(d);
+        pages.showPlane(0);
 
-        // Create the foreground panel
-        JPanel foregroundPanel = new JPanel();
-        foregroundPanel.setBackground(Color.BLUE);
-        foregroundPanel.setBounds(100, 100, 200, 200);
-        pages.add(foregroundPanel, Integer.valueOf(1)); // Add to layer 1
-        foregroundPanel.setVisible(false);
-
-
-        // Add the layered pane to the frame
         f.add(pages);
-
-        // Display the frame
         f.setVisible(true);
 
     }

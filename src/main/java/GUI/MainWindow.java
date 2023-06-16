5910e8d2ece0acf78c22d5c7fc706b614fa7f4e9
Anonym2546 <johannes.25406@gmail.com>
2023-06-16 20:06:51 +0200

    Added Placeholder and test with Icon-Switch

--- src/main/java/GUI/MainWindow.java
+++ src/main/java/GUI/MainWindow.java
@@ -4,7 +4,7 @@ import com.formdev.flatlaf.FlatDarkLaf;
 
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.event.*;
 
 public class MainWindow {
     JFrame f;
@@ -12,24 +12,20 @@ public class MainWindow {
         FlatDarkLaf.setup();
         f=new JFrame();//creating instance of JFrame
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.setSize(500, 500);
         f.setResizable(false);
         f.setLayout(null);
 
 
         StackedWidget pages = new StackedWidget(f);
-        //pages.setSize(500, 500);
-        pages.setBounds(f.getBounds());
-        StackedPane a = new StackedPane(pages);
-        a.setSize(700, 500);
-        a.setBackground(new Color(255,0,0));
-        StackedPane b = new StackedPane(pages);
+        StackedPane a = new StackedPane(pages, new Dimension(400, 500));
+        //a.setBackground(new Color(255,0,0));
+        StackedPane b = new StackedPane(pages, new Dimension(800, 500));
         StackedPane c = new StackedPane(pages);
         pages.registerPane(a);
         pages.registerPane(b);
         pages.registerPane(c);
-        a.setLayout(null);
-        BetterTextField d = new BetterTextField(new UITextFieldMethod() {
+        //a.setLayout(null);
+        BetterTextField dx = new BetterTextField(new UITextFieldMethod() {
             @Override
             public void performMethod(String data) {
                 System.out.println(data);
@@ -41,38 +37,53 @@ public class MainWindow {
                 System.out.println(data);
             }
         });
-        BetterTextField dx = new BetterTextField();
         BetterButton e = new BetterButton(new UIButtonMethod() {
             @Override
             public void performMethod() {
                 pages.showPlane(1);
-                f.setSize(500, 500);
+            }
+        });
+        p.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                // Call your function or perform the desired action
+                if (p.getText().equals("Password")) {
+                    p.setText("");
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (p.getText().isEmpty()) {
+                    p.setText("Password");
+                    p.setEchoChar((char)0);
+                }
+
             }
         });
         BetterButton x = new BetterButton(new UIButtonMethod() {
             @Override
             public void performMethod() {
                 pages.showPlane(0);
-                f.setSize(700, 500);
+                /*f.setSize(700, 500);
                 pages.setSize(700, 500);
-                System.out.println(pages.getSize());
+                System.out.println(pages.getSize());*/
             }
         });
-
         BetterButton i = new BetterButton(new UIButtonMethod() {
             @Override
             public void performMethod() {
                 if (p.getEchoChar() == 0) {
                     p.setEchoChar('•');
+                    i.setIcon(scaleIcon(new ImageIcon("src/main/java/GUI/icons/eye-slash.png"), 30, 30));
+                } else {
+                    p.setEchoChar((char) 0);
+                    i.setIcon(scaleIcon(new ImageIcon("src/main/java/GUI/icons/eye.png"), 30, 30));
                 }
-                else{
-                    p.setEchoChar((char)0);
-                }
-
             }
         });
-        dx.setEchoChar((char)0);
-        e.setText("Switch to next Page");
+
+        e.setText("Log in");
         Font font = new Font(dx.getFont().getFontName(), dx.getFont().getStyle(), 20);
         dx.setBounds(a.getWidth()/2-250/2-8, 80, 250, 30);
         dx.setFont(font);
@@ -88,13 +99,12 @@ public class MainWindow {
         e.setBounds(a.getWidth()/2-250/2-8, 220, 250, 30);
         a.registerComponent(e);
         b.registerComponent(x);
-        pages.showPlane(1);
+        pages.showPlane(0);
 
         f.add(pages);
         f.setVisible(true);
 
     }
-
     private static ImageIcon scaleIcon(ImageIcon icon, int width, int height) {
         Image image = icon.getImage();
         Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);

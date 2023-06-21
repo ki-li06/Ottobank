c8ba1d9f01c5cd2747806067fe00ee15c7174e65
fredi <frediuhl@gmail.com>
2023-06-21 13:56:55 +0200

    Konto, Sparkonto, Girokonto

--- src/main/java/Bank/Sparkonto.java
+++ src/main/java/Bank/Sparkonto.java
@@ -1,4 +1,16 @@
 package Bank;
 
 public class Sparkonto extends Konto{
+    double zinssatz = 0.03;
+
+    void Verzinsen(){}
+    double ZinssatzGeben(){return zinssatz;}
+    boolean Abheben (double abhebebetrag) {
+        if (KontostandGeben() - abhebebetrag < 0) {
+            kontostand = kontostand - abhebebetrag;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }

819b8ec80cb5ee066b274b622f27502d524f24f5
Anonym2546 <johannes.25406@gmail.com>
2023-06-21 13:44:56 +0200

    Added Register Page Widgets

--- src/main/java/GUI/MainWindow.java
+++ src/main/java/GUI/MainWindow.java
@@ -31,11 +31,11 @@ public class MainWindow{
         loginPage.registerComponent(infoLabel, "PRIVATE");
 
         //Username Text Field
-        BetterTextField usernameTextField = new BetterTextField();
-        usernameTextField.setEchoChar((char) 0);
-        usernameTextField.putClientProperty("JTextField.placeholderText", "Username");
-        usernameTextField.setBounds(loginPage.getWidth() / 2 - 250 / 2 - 8, 130, 250, 30);
-        loginPage.registerComponent(usernameTextField, "USERNAME_INPUT");
+        BetterTextField emailTextField = new BetterTextField();
+        emailTextField.setEchoChar((char) 0);
+        emailTextField.putClientProperty("JTextField.placeholderText", "Email");
+        emailTextField.setBounds(loginPage.getWidth() / 2 - 250 / 2 - 8, 130, 250, 30);
+        loginPage.registerComponent(emailTextField, "EMAIL_INPUT");
 
         //Password Text Field
         BetterTextField passwordTextField = new BetterTextField();
@@ -47,6 +47,12 @@ public class MainWindow{
         //Login Button
         BetterButton loginButton = new BetterButton();
         loginButton.setText("Log in");
+        loginButton.addMethod(new UIButtonMethod() {
+            @Override
+            public void performMethod() {
+                JOptionPane.showMessageDialog(null, "Test", "TEST", JOptionPane.ERROR_MESSAGE);
+            }
+        });
         loginButton.setBounds(loginPage.getWidth() / 2 - 250 / 2 - 8, 210, 250, 30);
         loginPage.registerComponent(loginButton, "LOGIN_BUTTON");
 
@@ -69,6 +75,60 @@ public class MainWindow{
         LabelHeight = registerInfoLabel.getPreferredSize().height;
         registerInfoLabel.setBounds(0, 40, 400, LabelHeight);
         registerPage.registerComponent(registerInfoLabel, "PRIVATE");
+
+
+        //vorname TextField
+        BetterTextField vornameTextField = new BetterTextField();
+        vornameTextField.setEchoChar((char) 0);
+        vornameTextField.putClientProperty("JTextField.placeholderText", "Vorname");
+        vornameTextField.setBounds(registerPage.getWidth() / 2 - 250 / 2 - 8, 130, 250, 30);
+        registerPage.registerComponent(vornameTextField, "VORNAME_INPUT");
+
+        //nachname TextField
+        BetterTextField nachnameTextField = new BetterTextField();
+        nachnameTextField.setEchoChar((char) 0);
+        nachnameTextField.putClientProperty("JTextField.placeholderText", "Nachname");
+        nachnameTextField.setBounds(registerPage.getWidth() / 2 - 250 / 2 - 8, 170, 250, 30);
+        registerPage.registerComponent(nachnameTextField, "NACHNAME_INPUT");
+
+        //email TextField
+        BetterTextField emailRegisterTextField = new BetterTextField();
+        emailRegisterTextField.setEchoChar((char) 0);
+        emailRegisterTextField.putClientProperty("JTextField.placeholderText", "Email");
+        emailRegisterTextField.setBounds(registerPage.getWidth() / 2 - 250 / 2 - 8, 210, 250, 30);
+        registerPage.registerComponent(emailRegisterTextField, "EMAIL_INPUT");
+
+        //pin TextField
+        BetterTextField pinRegisterTextField = new BetterTextField();
+        pinRegisterTextField.setEchoChar((char) 0);
+        pinRegisterTextField.putClientProperty("JTextField.placeholderText", "PIN");
+        pinRegisterTextField.setBounds(registerPage.getWidth() / 2 - 250 / 2 - 8, 250, 250, 30);
+        registerPage.registerComponent(pinRegisterTextField, "PIN_INPUT");
+
+        //pin2 TextField
+        BetterTextField pin2RegisterTextField = new BetterTextField();
+        pin2RegisterTextField.setEchoChar((char) 0);
+        pin2RegisterTextField.putClientProperty("JTextField.placeholderText", "PIN2");
+        pin2RegisterTextField.setBounds(registerPage.getWidth() / 2 - 250 / 2 - 8, 290, 250, 30);
+        registerPage.registerComponent(pin2RegisterTextField, "PIN2_INPUT");
+
+        //Register Button
+        BetterButton registerButton2 = new BetterButton();
+        registerButton2.setText("Register");
+        registerButton2.setBounds(loginPage.getWidth() / 2 - 250 / 2 - 8, 330, 250, 30);
+        registerPage.registerComponent(registerButton2, "PRIVATE");
+
+        //Back to Login Window Button
+        BetterButton loginButton2 = new BetterButton();
+        loginButton2.setText("Back to log in");
+        loginButton2.setBounds(loginPage.getWidth() / 2 - 250 / 2 - 8, 390, 250, 30);
+        loginButton2.addMethod(new UIButtonMethod() {
+            @Override
+            public void performMethod() {
+                layerManager.showPlane(0);
+            }
+        });
+        registerPage.registerComponent(loginButton2, "PRIVATE");
 
 
         layerManager.showPlane(0);

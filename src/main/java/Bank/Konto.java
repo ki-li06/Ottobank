473949d68947090597399677d5eb2ebb5ee51ad5
fredi <frediuhl@gmail.com>
2023-06-22 12:26:27 +0200

    Verzinsen, Kontonummer hinzugefügt

--- src/main/java/Bank/Konto.java
+++ src/main/java/Bank/Konto.java
@@ -1,13 +1,23 @@
 package Bank;
+import java.util.Random;
 
 public abstract class Konto {
-    protected double kontostand;
-    protected int kontonummer;
+    protected double kontostand = 100.00;
+    protected long kontonummer;
     protected Kunde besitzer;
+    Konto(){
+        Random random = new Random();
+        long number = random.nextLong();
+        number = Math.abs(number);
+        number = number % 10000000000L;
+        kontonummer = number;
+
+
+    }
 
     boolean Abheben(double betrag){return false;}
     int KontonummerGeben (){return 0;}
-    double KontostandGeben (){return 0;}
-    Kunde EigentümerGeben(){return null;}
+    double KontostandGeben (){return kontostand;}
+    String EigentümerGeben(){return besitzer.name;}
     void Einzahlen(double einzahlbetrag){}
 }
--- src/main/java/Bank/Sparkonto.java
+++ src/main/java/Bank/Sparkonto.java
@@ -1,9 +1,21 @@
 package Bank;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Sparkonto extends Konto{
     double zinssatz = 0.03;
 
-    void Verzinsen(){}
+    Sparkonto(){
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                kontostand = kontostand*(1+zinssatz);
+                System.out.println(kontostand);
+            }
+        }, 0, 60000);
+    }
     double ZinssatzGeben(){return zinssatz;}
     boolean Abheben (double abhebebetrag) {
         if (KontostandGeben() - abhebebetrag < 0) {

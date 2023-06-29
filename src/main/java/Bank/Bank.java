4182582a415932474a0994289821b1fa68618df0
Helene <helene.wery@gmx.de>
2023-06-29 20:58:26 +0200

    Methoden Nutzer-, Angestellte-, KundenGeben Methode Einzahlen in Konto.java

--- src/main/java/Bank/Bank.java
+++ src/main/java/Bank/Bank.java
@@ -6,16 +6,16 @@ import Bank.Konten.Sparkonto;
 import Bank.Nutzer.Angestellter;
 import Bank.Nutzer.Kunde;
 import Bank.Nutzer.Nutzer;
-import Datenbank.LiteSQL.KontenDB;
 import Datenbank.LiteSQL.NutzerDB;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
     public void NeuenKundenEinrichten(String name, String pin, String mail){
         Kunde kundeneu = new Kunde(name, mail, pin);
     }
-    public void NeuenAngestelltenEinrichten(String name, String pin, String mail, String passwort){
+    public void NeuenAngestelltenEinrichten(String name, String pin, String passwort, String mail){
         if (passwort.equals("admin")){
             Angestellter angestellterneu = new Angestellter(name, pin, mail);
         }
@@ -23,13 +23,17 @@ public class Bank {
             System.out.println("Das Passwort stimmt nicht mit dem Adminpasswort überein");
         }
     }
-    Sparkonto SparkontoEinrichten()
+    int SparkontoEinrichten()
     {
-        return new Sparkonto();
+        Sparkonto spark=new Sparkonto();
+        return spark.KontonummerGeben();
     }
-    Girokonto GirokontoEinrichten() {
-        return new Girokonto();
+    int GirokontoEinrichten()
+    {
+        Girokonto girok =new Girokonto();
+        return girok.KontonummerGeben();
     }
+
     boolean KontoLoeschen(Konto kontoLoeschen)
     {
         return true;
@@ -44,7 +48,7 @@ public class Bank {
         if (ndb.NutzerNameExistiert(name) == true){
             List<Nutzer> list = ndb.alleNutzerGeben();
             for (int i = 0; i < list.size(); i++) {
-                if(name.equals(list.get(i).getName())&& list.get(i) instanceof Angestellter){
+                if(name.equals(list.get(i).getName())){
                     return (Kunde) list.get(i);
                 }
             }
@@ -53,41 +57,62 @@ public class Bank {
         else{
             return null;
         }
-    }
+    };
     /*int [] KontonummernFuerKundenGeben(){
         NutzerDB ndb = new NutzerDB();
         ;
     };*/
-    Angestellter AngestellterSuchen(String name){
-        NutzerDB ndb = new NutzerDB();
-        if (ndb.NutzerNameExistiert(name) == true){
-            List<Nutzer> list = ndb.alleNutzerGeben();
-            for (int i = 0; i < list.size(); i++) {
-                if(name.equals(list.get(i).getName())&& list.get(i) instanceof Kunde){
-                    return (Angestellter) list.get(i);
-                }
-            }
-            return null;
+    Angestellter AngestellterSuchen(){
+        return null;
+    };
+    List<String> NutzernamenGeben()
+    {
+    NutzerDB ndb=new NutzerDB();
+    List<Nutzer> list=ndb.alleNutzerGeben();
+    List<String> ausgabe=new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            ausgabe.add(list.get(i).getName());
         }
-        else{
-            return null;
+        return ausgabe;
+    }
+    List<String> AngestelltennamenGeben()
+    {
+        NutzerDB ndb=new NutzerDB();
+        List<Nutzer> list=ndb.alleNutzerGeben();
+        List<String> ausgabe=new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) instanceof Angestellter)
+            { ausgabe.add(list.get(i).getName());}
         }
+        return ausgabe;
     }
-    /*String [] KundennamenGeben(){return null;};
-    String [] AngestelltennamenGeben(){return null;};*/
+    List<String> KundennamenGeben()
+    {
+        NutzerDB ndb=new NutzerDB();
+        List<Nutzer> list=ndb.alleNutzerGeben();
+        List<String> ausgabe=new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) instanceof Kunde)
+            { ausgabe.add(list.get(i).getName());}
+        }
+        return ausgabe;
+    }
 
-    void KundenLoeschen(String mail){
+
+    /*void KundenLoeschen(String name, String pin){
         NutzerDB ndb = new NutzerDB();
-        ndb.NutzerLöschen(mail);
-    }
+        if (ndb.PinRichtig(name, pin)){
+
+        }
+    };*/
 
 
+
+    Bank BankGeben(){return null;};//???
     Konto KontoSuchen(int kontonummer)
     {
-        KontenDB kdb = new KontenDB();
-        return kdb.getKontoVonKontonummer(kontonummer);
-    }
-
+        return null;
+    };
     void Beenden(){};
 
 

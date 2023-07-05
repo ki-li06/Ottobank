26ba31ca94b4d52f8cba61b1ddc48abd347d3870
fredi <frediuhl@gmail.com>
2023-07-05 23:42:10 +0200

    DBSchnittstelle gelöscht und Inhalt in Main gemacht nochmal

---
+++ src/main/java/Datenbank/LiteSQL/DBSchnittstelle.java
@@ -0,0 +1,27 @@
+package Datenbank.LiteSQL;
+import Bank.Konten.Konto;
+import Bank.Nutzer.Kunde;
+import Bank.Nutzer.Nutzer;
+import Datenbank.LiteSQL.NutzerDB;
+
+import static Bank.Nutzer.Angestellter.ANGESTELLTER;
+import static Bank.Nutzer.Kunde.KUNDE;
+
+
+public class DBSchnittstelle {
+    public static void main(String[] args) {
+        //Kunde k = new Kunde("Herbert Maier", "coole-mail@gmail.com", "notNull");
+        NutzerDB ndb = new NutzerDB();
+        //ndb.NutzerHinzufÃ¼gen(k);
+        ndb.AlleNutzerAusgeben();
+        String mail = "huber@sepp.com";
+        String pin = "123456";
+        boolean b = ndb.PasswortRichtig(mail, pin);
+        System.out.println("b: " + b);
+
+
+
+
+
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,88 @@
+import Bank.Nutzer.Kunde;
 import Datenbank.LiteSQL.NutzerDB;
+import GUI.*;
 
+import static Bank.Nutzer.Angestellter.ANGESTELLTER;
 import static util.Delay.delay;
 
 public class Main {
     public static void main(String[] args) {
-        //Kunde k = new Kunde("Herbert Maier", "coole-mail@gmail.com", "notNull");
-        NutzerDB ndb = new NutzerDB();
-        //ndb.NutzerHinzufügen(k);
-        ndb.AlleNutzerAusgeben();
-        String mail = "huber@sepp.com";
-        String pin = "123456";
-        boolean b = ndb.PasswortRichtig(mail, pin);
-        System.out.println("b: " + b);
+        MainWindow mw = WindowManager.getWindow();
+        //System.out.println(mw.getWindow());
+        mw.getWindow().showPlane("LOGIN_PAGE");
+        NutzerDB nutzerDB = new NutzerDB();
+        // LOGIN PAGE
+        mw.getWindow().showPlane("LOGIN_PAGE");
+        BetterTextField TLE = (BetterTextField) mw.getWindow().getFrame("LOGIN_PAGE").getElement("EMAIL_INPUT");
+        BetterTextField TLP = (BetterTextField) mw.getWindow().getFrame("LOGIN_PAGE").getElement("PIN_INPUT");
+
+        BetterButton BLL = (BetterButton) mw.getWindow().getFrame("LOGIN_PAGE").getElement("LOGIN_BUTTON");
+        BLL.addMethod(new UIButtonMethod() {
+            @Override
+            public void performMethod() {
+                //System.out.println("email: '" + TLE.getText() + "'");
+                //System.out.println("text : '" + TLP.getText() + "'");
+                if (nutzerDB.PasswortRichtig(TLE.getText(),TLP.getText())) {
+                    if(nutzerDB.NutzerZuMail(TLE.getText()).getType().equals(ANGESTELLTER)) {
+                        mw.getWindow().showPlane(4);
+                    }
+                    else {
+                        mw.getWindow().showPlane(2);
+                    }
+                } else {
+                    PopUp.showError("Passwort falsch!");
+                }
+
+            }
+        });
+        //REGISTER PAGE
+        BetterTextField TRV = (BetterTextField) mw.getWindow().getFrame("REGISTER_PAGE").getElement("VORNAME_INPUT");
+        BetterTextField TRN = (BetterTextField) mw.getWindow().getFrame("REGISTER_PAGE").getElement("NACHNAME_INPUT");
+        BetterTextField TRE = (BetterTextField) mw.getWindow().getFrame("REGISTER_PAGE").getElement("EMAIL_INPUT");
+        BetterTextField TRP = (BetterTextField) mw.getWindow().getFrame("REGISTER_PAGE").getElement("PIN_INPUT");
+        BetterButton BRR = (BetterButton) mw.getWindow().getFrame("REGISTER_PAGE").getElement("REGISTER_BUTTON");
+        BRR.addMethod(new UIButtonMethod() {
+            @Override
+            public void performMethod() {
+                if(TRV.getText().equals("") ||TRN.getText().equals("")|| TRE.getText().equals("") || TRP.getText().equals("")){
+                    String mail = TRE.getText();
+                    Kunde k = new Kunde(TRV.getText() +"\n"+ TRN.getText(),mail,TRP.getText());
+                    nutzerDB.NutzerHinzufügen(k);
+                    mw.getWindow().showPlane(1);
+                }else{
+                    PopUp.showError("Alle Eingaben müssen ausgefüllt sein!");
+                }
+            }
+        });
+        //SETTINGS PAGE
+        BetterTextField TSV = (BetterTextField) mw.getWindow().getFrame("SETTINGS_PAGE").getElement("VORNAME_INPUT");
+        BetterTextField TSN = (BetterTextField) mw.getWindow().getFrame("SETTINGS_PAGE").getElement("NACHNAME_INPUT");
+        BetterTextField TSE = (BetterTextField) mw.getWindow().getFrame("SETTINGS_PAGE").getElement("EMAIL_INPUT");
+        BetterTextField TSP = (BetterTextField) mw.getWindow().getFrame("SETTINGS_PAGE").getElement("PIN_INPUT");
+        BetterTextField TSC = (BetterTextField) mw.getWindow().getFrame("SETTINGS_PAGE").getElement("CONFIRM_PIN_INPUT");
+        BetterButton BSS = (BetterButton) mw.getWindow().getFrame("SETTINGS_PAGE").getElement("SAVE_BUTTON");
+
+
+        //MAIN PAGE USER
+        BetterButton BMS = (BetterButton) mw.getWindow().getFrame("REGISTER_PAGE").getElement("SETTINGS_BUTTON");
+
+
+        //MAIN PAGE ADMIN
+        BetterTextField TMAN = (BetterTextField) mw.getWindow().getFrame("ADMIN_MAIN_PAGE").getElement("USER_NAME");
+        BetterButton BMAL = (BetterButton) mw.getWindow().getFrame("ADMIN_MAIN_PAGE").getElement("LOGIN_BUTTON");
+        BMAL.addMethod(new UIButtonMethod() {
+            @Override
+            public void performMethod() {
+                if(TMAN.getText().equals("")){
+                    PopUp.showError("Alle Eingaben müssen ausgefüllt sein!");
+                }else{
+                    mw.getWindow().showPlane(2);
+                }
+            }
+        });
+    }
+
+    public static void main(MainWindow mw) {
+
     }
 }
